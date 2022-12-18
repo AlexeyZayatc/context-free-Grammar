7935ff5872cde93ffc36b4a0f4029244038791ce
Аден <bdthestar@yahoo.co.uk>
2022-12-18 18:03:09 +1000

    owo
    ну тут есть нфх?

--- greibach/test_greibach.py
+++ greibach/test_greibach.py
@@ -1,7 +1,15 @@
 import unittest
 
+from greibach.Grammar import Grammar
+from greibach.Greibach import Greibach
+
 
 class TestGreibach(unittest.TestCase):
-    def test_xxx(self):
-        # Enjoy testing :D
-        self.assertTrue(True)
+
+    def test_greibach_with_lambda(self):
+        self.assertRaises(Exception, lambda: Greibach(Grammar({'E': ['ε'],})))
+
+    def test_greibach_unified(self):
+        self.assertRaises(Exception, lambda: Grammar(
+            Greibach( {'E': ['TE', 'T'], 'T': ['FT', 'F'], 'F': ['(E)', 'a']}
+            )))
--- test_main.py
+++ test_main.py
@@ -40,24 +40,24 @@ class MyTestCase(unittest.TestCase):
 
         grammar_without_useless_symbols = original_grammar.remove_useless_symbols()
 
-        self.assertEqual(original_grammar,grammar_without_useless_symbols)
+        self.assertEqual(original_grammar, grammar_without_useless_symbols)
 
         test_case2: Grammar = Grammar(
-            {'S','A','B'},
-            {'a','b'},
-            {'S': ['a','A'], 'A': ['AB'], 'B': 'b'},
+            {'S', 'A', 'B'},
+            {'a', 'b'},
+            {'S': ['a', 'A'], 'A': ['AB'], 'B': 'b'},
             'S'
-            )
+        )
 
-        grammar_without_useless_symbols= test_case2.remove_useless_symbols()
+        grammar_without_useless_symbols = test_case2.remove_useless_symbols()
         test_case2_answer: Grammar = Grammar(
             {'S'},
             {'a'},
             {'S': ['a']},
             'S'
-            )
+        )
 
-        self.assertEqual(grammar_without_useless_symbols,test_case2_answer)
+        self.assertEqual(grammar_without_useless_symbols, test_case2_answer)
 
     def test_grammar_remove_unreachable_symbols(self):
         test_case1: Grammar = Grammar(
@@ -70,96 +70,94 @@ class MyTestCase(unittest.TestCase):
         )
 
         attemp = test_case1.remove_unreachable_symbols()
-        self.assertEqual(test_case1,attemp)
+        self.assertEqual(test_case1, attemp)
 
         test_case2: Grammar = Grammar(
-            {'S','A','B','C'},
-            {'1','2','3'},
+            {'S', 'A', 'B', 'C'},
+            {'1', '2', '3'},
             {
-            'S': ['A'],
-            'A': ['B'],
-            'B': ['C'],
+                'S': ['A'],
+                'A': ['B'],
+                'B': ['C'],
             },
             'S'
         )
         test_case2_answer: Grammar = Grammar(
-            {'S','A','B','C'},
+            {'S', 'A', 'B', 'C'},
             set(),
             {
-            'S': ['A'],
-            'A': ['B'],
-            'B': ['C'],
+                'S': ['A'],
+                'A': ['B'],
+                'B': ['C'],
             },
             'S'
-            )
-        attemp=test_case2.remove_unreachable_symbols()
-        self.assertEqual(test_case2_answer,attemp)
+        )
+        attemp = test_case2.remove_unreachable_symbols()
+        self.assertEqual(test_case2_answer, attemp)
 
         test_case3: Grammar = Grammar(
-            {'S','A','B','C'},
-            {'a','b','c'},
+            {'S', 'A', 'B', 'C'},
+            {'a', 'b', 'c'},
             {
-            'S': ['ab','a'],
-            'A': ['B'],
-            'B': ['C']
+                'S': ['ab', 'a'],
+                'A': ['B'],
+                'B': ['C']
             },
             'S'
-            )
+        )
         test_case3_answer: Grammar = Grammar(
             {'S'},
-            {'a','b'},
+            {'a', 'b'},
             {
-            'S': ['ab','a']
+                'S': ['ab', 'a']
             },
             'S'
-            )
+        )
         attemp = test_case3.remove_unreachable_symbols()
-        self.assertEqual(test_case3_answer,attemp)
+        self.assertEqual(test_case3_answer, attemp)
 
         test_case4: Grammar = Grammar(
-            {'S','A','B','C'},
-            {'a','b','c'},
+            {'S', 'A', 'B', 'C'},
+            {'a', 'b', 'c'},
             {
-            'S': ['S'],
-            'A': ['bBa'],
-            'B': ['cCa'],
-            'C': ['ccc']
+                'S': ['S'],
+                'A': ['bBa'],
+                'B': ['cCa'],
+                'C': ['ccc']
             },
             'S'
-            )
+        )
         test_case4_answer: Grammar = Grammar(
             {'S'},
             set(),
             {
-            'S': ['S']
+                'S': ['S']
             },
             'S'
-            )
+        )
         attemp = test_case4.remove_unreachable_symbols()
-        self.assertEqual(test_case4_answer,attemp)
+        self.assertEqual(test_case4_answer, attemp)
 
         test_case5: Grammar = Grammar(
-            {'E','F','T'},
-            {'+','*','(',')','a'},
+            {'E', 'F', 'T'},
+            {'+', '*', '(', ')', 'a'},
             {
-            'E': ['E+T','T'],
-            'F': ['(E)', 'a']
+                'E': ['E+T', 'T'],
+                'F': ['(E)', 'a']
             },
             'E'
-            )
+        )
         test_case5_answer: Grammar = Grammar(
-            {'E','T'},
+            {'E', 'T'},
             {'+'},
             {
-            'E': ['E+T','T'],
+                'E': ['E+T', 'T'],
             },
             'E'
-            )
+        )
         attemp = test_case5.remove_unreachable_symbols()
-        self.assertEqual(test_case5_answer,attemp)
-        
+        self.assertEqual(test_case5_answer, attemp)
 
-        
     def test_grammar_is_not_empty_when_its_not_empty(self):
         empty_grammar: Grammar = Grammar(
             {'A', 'B'},
@@ -171,17 +169,17 @@ class MyTestCase(unittest.TestCase):
             'A'
         )
         empty_grammar_terminal_chain_achieved: Grammar = Grammar(
-            {'A','B','S','C','D'},  
-            {'a','b','c','d'},
+            {'A', 'B', 'S', 'C', 'D'},
+            {'a', 'b', 'c', 'd'},
             {'S': ['aAa'], 'A': ['bBb'], 'B': ['cCc'], 'C': ['dDd'], 'D': ['abcd']},
             'S'
-            )
+        )
         empty_grammar_terminal_chain_with_extra_routes: Grammar = Grammar(
-            {'A','B','S','C','D', 'F'},
-            {'a','b','c','d'},
+            {'A', 'B', 'S', 'C', 'D', 'F'},
+            {'a', 'b', 'c', 'd'},
             {'S': ['aAa'], 'A': ['bBb', 'Cdd'], 'B': ['cDc'], 'C': ['F'], 'D': ['abcd']},
             'S'
-            )
+        )
         self.assertTrue(empty_grammar.is_not_empty())
         self.assertTrue(empty_grammar_terminal_chain_achieved.is_not_empty())
         self.assertTrue(empty_grammar_terminal_chain_with_extra_routes.is_not_empty())
@@ -191,33 +189,33 @@ class MyTestCase(unittest.TestCase):
             {'A'},
             {'0'},
             {},
-             'A'
-            )
+            'A'
+        )
         empty_grammar_no_axiom_rule: Grammar = Grammar(
-            {'A','B','S'},
-            {'a','b','c'},
-            {'A' : [['a']], 'B': [['b','c','A']]},
+            {'A', 'B', 'S'},
+            {'a', 'b', 'c'},
+            {'A': [['a']], 'B': [['b', 'c', 'A']]},
             'S'
-            )
+        )
         empty_grammar_no_terminal_chain: Grammar = Grammar(
-            {'A','B','S','C','D'},
-            {'a','b','c','d'},
+            {'A', 'B', 'S', 'C', 'D'},
+            {'a', 'b', 'c', 'd'},
             {'S': ['aAa'], 'A': ['bBb'], 'B': ['cCc'], 'C': ['dDd']},
             'S'
-            )
+        )
         empty_grammar_no_terminal_chain_recursion: Grammar = Grammar(
-            {'A','B','S','C','D'},
-            {'a','b','c','d'},
+            {'A', 'B', 'S', 'C', 'D'},
+            {'a', 'b', 'c', 'd'},
             {'S': ['aAa'], 'A': ['bBb'], 'B': ['cCc'], 'C': ['dAd']},
             'S'
-            )
+        )
         empty_grammar_no_rule_to_good_symbol: Grammar = Grammar(
-            {'A','B','C','S'},
-            {'a','b','c'},
-            {'S': ['AB'], 'C': ['aaa'], 'A':['BBB']},
+            {'A', 'B', 'C', 'S'},
+            {'a', 'b', 'c'},
+            {'S': ['AB'], 'C': ['aaa'], 'A': ['BBB']},
             'S'
-            )
-        empty_grammar_lambda_rule: Grammar = Grammar({'A',}, {'0',}, {'A': ['']}, 'A')
+        )
+        empty_grammar_lambda_rule: Grammar = Grammar({'A', }, {'0', }, {'A': ['']}, 'A')
         self.assertFalse(empty_grammar.is_not_empty())
         self.assertFalse(empty_grammar_no_axiom_rule.is_not_empty())
         self.assertFalse(empty_grammar_no_terminal_chain.is_not_empty())
@@ -228,33 +226,33 @@ class MyTestCase(unittest.TestCase):
     def test_grammar_is_not_empty_grammar_output(self):
         test_case1: Grammar = Grammar(
             {'S', 'A', 'B', 'C', 'D', 'E', 'F', 'G'},
-            {'a','b','c','d','e','f','g'},
+            {'a', 'b', 'c', 'd', 'e', 'f', 'g'},
             {
-                'S': ['aAa','bbBbb','abcd'],
+                'S': ['aAa', 'bbBbb', 'abcd'],
                 'A': ['cCc', 'dDd'],
                 'B': ['bDb', 'Eee'],
-                'C': ['ccc','G'],
+                'C': ['ccc', 'G'],
                 'D': ['eeEee'],
                 'E': ['gGg'],
                 'G': ['G'],
                 'F': ['fffAAA']
-                },
+            },
             'S'
-            )
+        )
         test_case1_answer: Grammar = Grammar(
             {'S', 'A', 'C', 'F'},
-            {'a','b','c','d','e','f','g'},
+            {'a', 'b', 'c', 'd', 'e', 'f', 'g'},
             {
-                'S': ['aAa','abcd'],
+                'S': ['aAa', 'abcd'],
                 'A': ['cCc'],
                 'C': ['ccc'],
                 'F': ['fffAAA']
-                },
+            },
             'S'
-            )
-        test_case1=test_case1.remove_bad_non_terminals_and_rules()
-        self.assertEqual(test_case1,test_case1_answer)
-        
+        )
+        test_case1 = test_case1.remove_bad_non_terminals_and_rules()
+        self.assertEqual(test_case1, test_case1_answer)
+
+
 if __name__ == '__main__':
     unittest.main()
-        
